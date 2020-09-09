fce79855b829904f427d7fb26b1eebd993873556
bijeshbasnet <bijeshjungbasnet@gmail.com>
2020-09-09 17:59:24 +0545

    Full text search with PostgreSQl SearchVector

--- blog/urls.py
+++ blog/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
          views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/',
          views.post_list, name='post_list_by_tag'),
-    path('feed/', LatestPostsfeed(), name='post_feed')
+    path('feed/', LatestPostsfeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
 ]
--- mysite/settings.py
+++ mysite/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'blog.apps.BlogConfig',
@@ -77,8 +78,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Blog',
+        'USER': 'Bijesh',
+        'PASSWORD': 'bijesh',
+        'HOST': 'localhost'
     }
 }
 
